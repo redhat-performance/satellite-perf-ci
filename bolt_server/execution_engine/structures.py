9167d2ff50315c2e9cc14466011ac943b9f1a992
Saurabh Badhwar <sbadhwar@redhat.com>
2017-10-10 17:51:56 +0530

    Add a task queue structure to maintain the task state
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- bolt_server/execution_engine/structures.py
+++ bolt_server/execution_engine/structures.py
@@ -46,3 +46,86 @@ class Task(object):
 
 class TaskQueue(object):
     """Create and queue a new task for execution"""
+
+    #Task lifecycle status
+    TASK_QUEUED = 0
+    TASK_PENDING = 1
+    TASK_RUNNING = 2
+    TASK_HALTED = 3
+    TASK_COMPLETE = 4
+
+    def __init__(self):
+        """Initialize the task queue structure."""
+
+        self.task_queue = {}
+
+    def queue_task(self, task_name, task_struct, task_params, task_topics, task_dependency=None):
+        """Queue a new task
+
+        Keyword arguments:
+        task_name -- The name of the task
+        task_struct -- The task structure
+        task_params -- The parameters to be passed to the task structure
+        task_topic -- The topics the task should be broadcasted to
+        task_dependency -- The tasks on which the current task depends
+
+        Returns:
+            task_id The id of the task
+        """
+
+        task = Task(task_name, task_struct, task_params, task_topics)
+        task_id = task.get_task_id()
+        self.task_queue[task_id] = [self.TASK_QUEUED, task_dependency]
+        return task_id
+
+    def get_task(self, task_id):
+        """Return the task based on its task id
+
+        Keyword arguments:
+        task_id -- The id of the task
+
+        Raises:
+            KeyError if the task is not present
+
+        Returns:
+            List
+        """
+
+        if task_id not in self.task_queue.keys():
+            raise KeyError("The mentioned task has not been queued")
+
+        return self.task_queue[task_id][0].get_task()
+
+    def get_task_status(self, task_id):
+        """Get the status of the current task
+
+        Keyword arguments:
+        task_id -- The id of the task to be queried for the status
+
+        Raises:
+            KeyError if the task id is not present
+
+        Returns:
+            Integer The status of the task
+        """
+
+        if task_id not in self.task_queue.keys():
+            raise KeyError("The provided task is not queued")
+
+        return self.task_queue[task_id][1]
+
+    def change_task_status(self, task_id, task_status):
+        """Change the status of the task
+
+        Keyword arguments:
+        task_id -- The task id of the task to update the status of
+        task_status -- The new status value to be set for the task
+
+        Raises:
+            KeyError if the task is not queued
+        """
+
+        if task_id not in self.task_queue.keys():
+            raise KeyError("The provided task is not present")
+
+        self.task_queue[task_id][0] = task_status
